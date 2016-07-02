150a15011d1de617ac5fc511917cca3968e4580b
aumel <aurelienmelot@gmail.com>
2016-07-02 19:53:46 +0200

    Add circle CI

---
+++ tests/test-runnner/runner.js
@@ -0,0 +1,118 @@
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+
+// The magic prefix for special log output
+// Must match packages/test-in-console/driver.js
+var MAGIC_PREFIX = '##_meteor_magic##';
+var MAGIC_PREFIX_REGEX = new RegExp(MAGIC_PREFIX);
+
+var options = new chrome.Options();
+var logOptions = new webdriver.logging.Preferences();
+logOptions.setLevel('browser', webdriver.logging.Level.ALL);
+options.setLoggingPrefs(logOptions);
+
+var driver = new chrome.Driver(options);
+
+var xunitEntries = [];
+
+function magicEntry(facility, message) {
+  if (facility === 'xunit') {
+    xunitEntries.push(message);
+  }
+  else if (facility === 'state') {
+    // Ignoring.
+  }
+  else {
+    console.log("    [Unknown facility: " + facility + "] " + message);
+  }
+}
+
+function processMagicMessage(message) {
+  var regex = /([^\s]*)\s*([^\s]*)\s*(.*)/i;
+  var match = regex.exec(message);
+  if (!match) {
+    console.log("Unknown console.log message format: " + message);
+    return;
+  }
+  message = match[3];
+  message = message.substring(MAGIC_PREFIX.length);
+  var colonIndex = message.indexOf(': ');
+  if (colonIndex === -1) {
+    magicEntry('', message);
+  }
+  else {
+    var facility = message.substring(0, colonIndex);
+    message = message.substring(colonIndex + 2);
+    magicEntry(facility, message);
+  }
+}
+
+function processLogEntry(entry) {
+  if (MAGIC_PREFIX_REGEX.test(entry.message)) {
+    processMagicMessage(entry.message);
+  }
+  else {
+    console.log("    [" + entry.level.name + "] " + entry.message);
+  }
+}
+
+function storeResult() {
+  if (!process.env.CIRCLE_TEST_REPORTS) return;
+
+  var baseDirectory = path.join(process.env.CIRCLE_TEST_REPORTS, process.env.PACKAGE || 'unknown');
+  var xunitOutputFile = path.join(baseDirectory, 'test-results.xml');
+  console.log("  > Writing xunit output to: " + xunitOutputFile);
+  mkdirp.sync(baseDirectory);
+  fs.writeFileSync(xunitOutputFile, xunitEntries.join('\n'));
+}
+
+console.log("  > Opening Meteor test suite...");
+driver.get('http://127.0.0.1:4096/xunit').then(function() {
+  console.log("  > Running tests...");
+
+  // Wait for tests to complete.
+  var pollTimer = setInterval(function() {
+    // Output logs while the tests are running.
+    driver.manage().logs().get('browser').then(function (log) {
+      for (var index in log) {
+        var entry = log[index];
+        processLogEntry(entry);
+      }
+    });
+
+    driver.executeScript(function() {
+      if (typeof TEST_STATUS !== 'undefined')
+        return TEST_STATUS.DONE;
+      return typeof DONE !== 'undefined' && DONE;
+    }).then(function(done) {
+      if (done) {
+        clearInterval(pollTimer);
+        driver.executeScript(function () {
+          if (typeof TEST_STATUS !== 'undefined')
+            return TEST_STATUS.FAILURES;
+          if (typeof FAILURES === 'undefined') {
+            return 1;
+          }
+          return 0;
+        }).then(function (failures) {
+          // Output final logs.
+          driver.manage().logs().get('browser').then(function (log) {
+            for (var index in log) {
+              var entry = log[index];
+              processLogEntry(entry);
+            }
+
+            driver.quit().then(function() {
+              storeResult();
+              console.log("  > Tests completed " + (failures ? "WITH FAILURES" : "OK") + ".");
+              process.exit(failures ? 1 : 0);
+            });
+          });
+        });
+      }
+    });
+  }, 500);
+});
---
+++ tests/test-runnner/test-all.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# On Mac OS X GNU grep is ggrep.
+if hash ggrep 2>/dev/null; then
+  GREP='ggrep'
+else
+  GREP='grep'
+fi
+
+# Packages with tests. To ignore a package, place // NOTEST after the onTest definition.
+PACKAGES=$( $GREP -Prl 'Package\.(onTest|on_test)(?!.*// NOTEST)' packages | $GREP -Po 'packages/\K.*(?=/)' | sort -u )
+
+SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
+
+PACKAGE_DIRS=${PACKAGE_DIRS:-packages}
+export PACKAGE_DIRS
+
+if [ -n "$CIRCLE_NODE_TOTAL" ] ; then
+  i=0
+  PACKAGES_TO_TEST=()
+  for pkg in ${PACKAGES}; do
+    if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
+    then
+      PACKAGES_TO_TEST+=" ${pkg}"
+    fi
+    ((i=i+1))
+  done
+else
+  PACKAGES_TO_TEST="$PACKAGES"
+fi
+
+# Perform tests.
+TESTS_FAILED=0
+PACKAGES_FAILED=""
+for pkg in ${PACKAGES_TO_TEST}; do
+  echo ">>> Testing package '${pkg}'..."
+  "${SCRIPT_DIR}/test-package.sh" packages/${pkg} || {
+    echo "ERROR: Tests for package '${pkg}' failed."
+    TESTS_FAILED=1
+    PACKAGES_FAILED="${PACKAGES_FAILED} ${pkg}"
+  }
+done
+
+if [ "${TESTS_FAILED}" == "1" ]; then
+  echo "ERROR: Some of the packages have failing tests:"
+  echo "ERROR: ${PACKAGES_FAILED}"
+  echo "ERROR: For details see output above."
+fi
+
+exit ${TESTS_FAILED}
